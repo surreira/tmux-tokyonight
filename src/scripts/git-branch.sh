d67a7f0eac1f407d4126dfa18717b401176cb245
Abel Surreira <surreira@gmail.com>
2023-12-31 17:32:13 +0000

    feat: day tokyonight sub-theme

--- src/scripts/git-branch.sh
+++ src/scripts/git-branch.sh
@@ -2,13 +2,27 @@
 
 cd $1
 
+# Default THEME only changes when this script has a second argument value
+THEME=${2:-light}
+
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 STATUS=$(git status --porcelain 2>/dev/null | egrep "^(M| M)" | wc -l | xargs)
 
+# Default colors (light mode)
+COLOR="#e9e9ec"
+COLOR_OK="#2e7de9"
+COLOR_NOK="#f7768e"
+
+# Set colors based on THEME argument
+if [ $THEME == "dark" ]; then
+    COLOR="#1f2335"
+    COLOR_OK="#7aa2f7"
+fi
+
 if [[ -n "$BRANCH" ]]; then
     if [[ "$STATUS" = "0" ]]; then
-        echo "#[fg=#1f2335,bg=#1f2335,nobold,nounderscore,noitalics]#[fg=#7aa2f7,bg=#1f2335]  $BRANCH "
+        echo "#[fg=$COLOR,bg=$COLOR,nobold,nounderscore,noitalics]#[fg=$COLOR_OK,bg=$COLOR]  $BRANCH "
     else
-        echo "#[fg=#1f2335,bg=#1f2335,nobold,nounderscore,noitalics]#[fg=#f7768e,bg=#1f2335]  $BRANCH "
+        echo "#[fg=$COLOR,bg=$COLOR,nobold,nounderscore,noitalics]#[fg=$COLOR_NOK,bg=$COLOR]  $BRANCH "
     fi
 fi
--- tokyonight.tmux
+++ tokyonight.tmux
@@ -3,6 +3,11 @@
 TOKYONIGHT_THEME="src/tokyonight-storm.conf"
 CURRENT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+COLOR=${TMUX_COLOR_MODE:-dark} # default value "dark"
+if [[ $COLOR == "light" ]]; then
+    TOKYONIGHT_THEME="src/tokyonight-day.conf"
+fi
+
 main() {
     tmux source-file "$CURRENT_DIRECTORY/$TOKYONIGHT_THEME"
 }
